e53837c507ed240a8f1f3314d595e95087f73bfe
kumarsatish23 <kumarsatish.skr@gmail.com>
2023-04-30 21:26:43 +0530

    initial commit

--- src/main/java/in/vanna/restController/SparkFileController.java
+++ src/main/java/in/vanna/restController/SparkFileController.java
@@ -1,62 +1,42 @@
 package in.vanna.restController;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+
+import in.vanna.service.SparkFileService;
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.spark.api.java.JavaPairRDD;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
-
-import in.vanna.service.SparkFileService;
-
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
 
 @RestController
 public class SparkFileController {
 
 	@Autowired
 	SparkFileService fileService;
-	
-	@PostMapping(value = "/countLine", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Long countLine(@RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
-		
-        long response = fileService.processData(file);
-        return response;
-    }
-	
-	@PostMapping("/countLines")
-    public ResponseEntity<Long> countLines(@RequestParam("file") MultipartFile file) {
-        // Initialize Spark
-        SparkConf conf = new SparkConf().setAppName("Line Count").setMaster("local[*]");
-        JavaSparkContext sc = new JavaSparkContext(conf);
 
-        // Create a temporary file to store the uploaded file
-        File tempFile;
-        try {
-            tempFile = Files.createTempFile("temp", null).toFile();
-            FileCopyUtils.copy(file.getBytes(), tempFile);
-        } catch (IOException e) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+	@PostMapping(value = "/countLines", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<Object> countLines(@RequestParam("file") MultipartFile file) throws IOException {
+		Map<String, Object> response = fileService.processData(file);
+		if (response.containsKey("Error")) {
+			return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		} else
+			return new ResponseEntity<>(response, HttpStatus.OK);
+	}
 
-        // Load the file
-        JavaRDD<String> lines = sc.textFile(tempFile.getAbsolutePath());
+	@PostMapping(value = "/bar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public Map<String, Long> bar(@RequestParam("file") MultipartFile file) throws IOException {
+		return fileService.bar(file);
 
-        // Count the lines
-        long count = lines.count();
-
-        // Stop Spark and delete the temporary file
-        sc.stop();
-        tempFile.delete();
-
-        // Return the result
-        return new ResponseEntity<>(count, HttpStatus.OK);
-    }
+	}
+	@PostMapping(value="/hello")
+	public String hello() {
+		return "hello world";
+	}
 }
--- src/main/java/in/vanna/service/SparkFileService.java
+++ src/main/java/in/vanna/service/SparkFileService.java
@@ -1,21 +1,73 @@
 package in.vanna.service;
 
+import java.io.File;
 import java.io.IOException;
-
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
+import scala.Tuple2;
 
 @Service
 public class SparkFileService {
 
-    public long processData(MultipartFile file) throws IOException {
-        SparkConf conf = new SparkConf().setAppName("Data Processor").setMaster("local[*]");
-        try (var sparkContext = new JavaSparkContext(conf)) {
-			final var myRdd=sparkContext.textFile(file.getResource().getURI().getRawPath());
-			
-			return myRdd.count();
+	public Map<String, Object> processData(MultipartFile file) throws IOException {
+		SparkConf conf = new SparkConf().setAppName("Line Count").setMaster("local[*]");
+		try (JavaSparkContext sparkContext = new JavaSparkContext(conf)) {
+			File tempFile;
+			Map<String, Object> response = new HashMap<>();
+			try {
+				tempFile = Files.createTempFile("temp", file.getOriginalFilename()).toFile();
+				FileCopyUtils.copy(file.getBytes(), tempFile);
+			} catch (IOException e) {
+				response.put("Error", e);
+				return response;
+			}
+			JavaRDD<String> lines = sparkContext.textFile(tempFile.getAbsolutePath());
+			response.put("Number Of Lines", lines.count());
+			response.put("content", lines.take(10));
+			final var filteredWords = sparkContext.textFile(tempFile.getAbsolutePath())
+					.map(line -> line.replaceAll("[^a-zA-Z\\s]", "").toLowerCase())
+					.flatMap(line -> List.of(line.split("\\s")).iterator())
+					.filter(word -> ((word != null) && (word.trim().length() > 0)));
+			final var counts = filteredWords.mapToPair(word -> new Tuple2<>(word, 1L)).reduceByKey(Long::sum);
+			response.put("repeting linses",
+					counts.mapToPair(tuple -> new Tuple2<>(tuple._2, tuple._1)).sortByKey(false).take(10));
+
+			sparkContext.stop();
+			tempFile.delete();
+			return response;
 		}
-    }
+	}
+
+	public Map<String, Long> bar(MultipartFile file) {
+		SparkConf conf = new SparkConf().setAppName("Line Count").setMaster("local[*]");
+		try (JavaSparkContext sparkContext = new JavaSparkContext(conf)) {
+			File tempFile;
+			Map<String,Long> response = new HashMap<>();
+			try {
+				tempFile = Files.createTempFile("temp", file.getOriginalFilename()).toFile();
+				FileCopyUtils.copy(file.getBytes(), tempFile);
+			} catch (IOException e) {
+				response.put( "Error",0L);
+				return response;
+			}
+			final var filteredWords = sparkContext.textFile(tempFile.getAbsolutePath())
+					.map(line -> line.replaceAll("[^a-zA-Z\\s]", "").toLowerCase())
+					.flatMap(line -> List.of(line.split("\\s")).iterator())
+					.filter(word -> ((word != null) && (word.trim().length() > 0)));
+			final var counts = filteredWords.mapToPair(word -> new Tuple2<>(word, 1L)).reduceByKey(Long::sum);
+			Map<String, Long> collectAsMap = counts.mapToPair(tuple -> new Tuple2<>(tuple._1 , tuple._2 )).sortByKey().collectAsMap();
+			sparkContext.stop();
+			tempFile.delete();
+			return  collectAsMap;
+		}
+	}
 }
